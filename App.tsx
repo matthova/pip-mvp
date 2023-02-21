fac5073a114d78a9917236f0070204e433dee69e
Matt Hova <matthova87@gmail.com>
2023-02-20 21:10:49 -0800

    mvp position is working

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Dimensions, StyleSheet, ScaledSize } from "react-native";
+import { View, StyleSheet, LayoutRectangle } from "react-native";
 import Animated, {
   useSharedValue,
   useDerivedValue,
@@ -7,30 +7,28 @@ import Animated, {
   useAnimatedGestureHandler,
   withSpring,
 } from "react-native-reanimated";
+import usePrevious from "react-use/esm/usePrevious";
 import * as SafeArea from "react-native-safe-area-context";
 import {
   PanGestureHandler,
   PanGestureHandlerGestureEvent,
 } from "react-native-gesture-handler";
+import * as ScreenOrientation from "expo-screen-orientation";
 
-function ChatHeads({
-  children,
-}: React.PropsWithChildren<Record<never, never>>) {
-  const [windowWidth, setWindowWidth] = React.useState(
-    Dimensions.get("window").width
-  );
-  const [windowHeight, setWindowHeight] = React.useState(
-    Dimensions.get("window").height
-  );
-
+function ChatHeads() {
+  React.useEffect(() => {
+    ScreenOrientation.unlockAsync();
+  }, []);
   const transX = useSharedValue(0);
   const transY = useSharedValue(0);
-  const {
-    top: topOffset,
-    bottom: bottomOffset,
-    left: leftOffset,
-    right: rightOffset,
-  } = SafeArea.useSafeAreaInsets();
+  const transXDestination = useSharedValue(0);
+  const transYDestination = useSharedValue(0);
+  const [dimensions, setDimensions] = React.useState<LayoutRectangle | null>(
+    null
+  );
+  const dimensionsDv = useDerivedValue(() => dimensions, [dimensions]);
+  const dimensionsPrev = usePrevious(dimensions);
+  console.log("dimensions", dimensions);
 
   type AnimatedGHContext = {
     startX: number;
@@ -38,64 +36,63 @@ function ChatHeads({
   };
 
   React.useEffect(() => {
-    function handleDimensionChange({
-      window,
-      screen: _screen,
-    }: {
-      window: ScaledSize;
-      screen: ScaledSize;
-    }) {
-      if (window.width !== windowWidth) {
-        setWindowWidth(window.width);
-        transX.value = 0; // TODO
-      }
-      if (window.height !== windowHeight) {
-        setWindowHeight(window.height);
-        transY.value = 0; // TODO
-      }
+    if (dimensions == null || dimensionsPrev == null) {
+      return;
     }
-    Dimensions.addEventListener("change", handleDimensionChange);
-    // No remove event listener?
-  }, [transX, transY, windowHeight, windowWidth]);
 
-  const windowWidthDv = useDerivedValue(() => windowWidth, [windowWidth]);
-  const windowHeightDv = useDerivedValue(() => windowHeight, [windowHeight]);
-  const topOffsetDv = useDerivedValue(() => topOffset, [topOffset]);
-  const bottomOffsetDv = useDerivedValue(() => bottomOffset, [bottomOffset]);
-  const leftOffsetDv = useDerivedValue(() => leftOffset, [leftOffset]);
-  const rightOffsetDv = useDerivedValue(() => rightOffset, [rightOffset]);
+    const oldWidth = dimensionsPrev.width - styles.head.width;
+    const newWidth = dimensions.width - styles.head.width;
+    const newX = (transXDestination.value / oldWidth) * newWidth;
+
+    const oldHeight = dimensionsPrev.height - styles.head.height;
+    const newHeight = dimensions.height - styles.head.height;
+    const newY = (transYDestination.value / oldHeight) * newHeight;
+
+    transX.value = withSpring(newX, {
+      damping: 200,
+      stiffness: 200,
+    });
+    transXDestination.value = newX;
+    transY.value = withSpring(newY, {
+      damping: 200,
+      stiffness: 200,
+    });
+    transYDestination.value = newY;
+  }, [
+    dimensions,
+    dimensionsPrev,
+    transX,
+    transXDestination,
+    transY,
+    transYDestination,
+  ]);
 
   const gestureHandler = useAnimatedGestureHandler<
     PanGestureHandlerGestureEvent,
     AnimatedGHContext
   >({
     onStart: (_, ctx) => {
-      ctx.startX = transX.value;
-      ctx.startY = transY.value;
+      ctx.startX = transXDestination.value;
+      ctx.startY = transYDestination.value;
     },
     onActive: (event, ctx) => {
       transX.value = ctx.startX + event.translationX;
+      transXDestination.value = transX.value;
       transY.value = ctx.startY + event.translationY;
+      transYDestination.value = transY.value;
     },
     onEnd: (event) => {
-      const width =
-        windowWidthDv.value -
-        leftOffsetDv.value -
-        rightOffsetDv.value -
-        styles.container.margin * 2 -
-        styles.head.width; // minus margins & width
-      const height =
-        windowHeightDv.value -
-        topOffsetDv.value -
-        bottomOffsetDv.value -
-        styles.container.margin * 2 -
-        styles.head.height;
-
+      if (dimensionsDv.value == null) {
+        return;
+      }
       const toss = 0.1;
       function clamp(value: number, min: number, max: number) {
         return Math.min(Math.max(value, min), max);
       }
+      const width = dimensionsDv.value.width - styles.head.width;
       const targetX = clamp(transX.value + toss * event.velocityX, 0, width);
+
+      const height = dimensionsDv.value.height - styles.head.height;
       const targetY = clamp(transY.value + toss * event.velocityY, 0, height);
 
       const top = targetY;
@@ -128,11 +125,13 @@ function ChatHeads({
         damping: 40,
         stiffness: 90,
       });
+      transXDestination.value = snapX;
       transY.value = withSpring(snapY, {
         velocity: event.velocityY,
         damping: 40,
         stiffness: 90,
       });
+      transYDestination.value = snapY;
     },
   });
 
@@ -150,37 +149,36 @@ function ChatHeads({
   });
 
   return (
-    <PanGestureHandler onGestureEvent={gestureHandler}>
-      <Animated.View style={[styles.headContainer, stylez]}>
-        {children}
-      </Animated.View>
-    </PanGestureHandler>
+    <View
+      style={styles.container}
+      onLayout={(e) => setDimensions(e.nativeEvent.layout)}
+    >
+      <PanGestureHandler onGestureEvent={gestureHandler}>
+        <Animated.View style={[styles.headContainer, stylez]}>
+          <View style={styles.head} />
+        </Animated.View>
+      </PanGestureHandler>
+    </View>
   );
 }
 
 function Main(): React.ReactElement {
   return (
-    <SafeArea.SafeAreaProvider>
-      <SafeArea.SafeAreaView style={styles.safeArea}>
-        <View style={styles.container}>
-          <ChatHeads>
-            <View style={styles.head} />
-          </ChatHeads>
-        </View>
-      </SafeArea.SafeAreaView>
-    </SafeArea.SafeAreaProvider>
+    <SafeArea.SafeAreaView style={styles.safeArea}>
+      <ChatHeads />
+    </SafeArea.SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   safeArea: {
     flex: 1,
+    backgroundColor: "#DDDDFF",
   },
   container: {
     margin: 50,
     flex: 1,
-    borderWidth: 1,
-    borderColor: "pink",
+    backgroundColor: "#FFDDDD",
   },
   head: {
     width: 80,
@@ -193,4 +191,15 @@ const styles = StyleSheet.create({
   },
 });
 
-export default Main;
+function Providers() {
+  React.useEffect(() => {
+    ScreenOrientation.unlockAsync();
+  }, []);
+
+  return (
+    <SafeArea.SafeAreaProvider>
+      <Main />
+    </SafeArea.SafeAreaProvider>
+  );
+}
+export default Providers;
